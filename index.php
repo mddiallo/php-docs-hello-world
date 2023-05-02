116ff18bdc9e1c0d81fadd632b97d49ddba98b2b
Mouhamadou DIALLO <mouhamadou.diallo@hotmail.com>
2023-05-02 16:32:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
-<?php
-
-echo "Hello World !!!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        /* Define the three colors for the rows */
+        .color1 {
+            background-color: #ffcccc;
+        }
+        .color2 {
+            background-color: #ccffcc;
+        }
+        .color3 {
+            background-color: #ccccff;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr class="color1">
+            <td><?php echo "Hello World"; ?></td>
+        </tr>
+        <tr class="color2">
+            <td><?php echo "Hello World"; ?></td>
+        </tr>
+        <tr class="color3">
+            <td><?php echo "Hello World"; ?></td>
+        </tr>
+        <tr class="color1">
+            <td><?php echo "Hello World"; ?></td>
+        </tr>
+        <tr class="color2">
+            <td><?php echo "Hello World"; ?></td>
+        </tr>
+        <tr class="color3">
+            <td><?php echo "Hello World"; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
