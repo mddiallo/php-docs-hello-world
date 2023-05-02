5c54968654bf96d080904385fa717430402a0359
Mouhamadou DIALLO <mouhamadou.diallo@hotmail.com>
2023-05-02 17:07:24 +0200

    Update index.php
    add timer to show the time to load the page

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
     </style>
 </head>
 <body>
+    <?php
+        // Start the timer
+        $start = microtime(true);
+        
+        // Your code for generating the table goes here
+        // ...
+    ?>
+
     <table>
         <tr class="color1">
             <td><?php echo "Hello World"; ?></td>
@@ -35,5 +43,14 @@
             <td><?php echo "Hello World"; ?></td>
         </tr>
     </table>
+
+    <?php
+        // End the timer and calculate the page load time
+        $end = microtime(true);
+        $loadTime = $end - $start;
+        
+        // Display the page load time
+        echo "Page loaded in " . number_format($loadTime, 4) . " seconds.";
+    ?>
 </body>
 </html>
